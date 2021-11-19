ad049493567b98fbeaa901a00a15c4c8f22ed0a8
Stian Soiland-Reyes <stain@apache.org>
2021-11-19 15:39:50 +0000

    Don't break .pdf hyperlinks

--- build/build.sh
+++ build/build.sh
@@ -20,7 +20,7 @@ ln -s ../latex/ro-crate.bib content/manual-references-ro-crate.bib
 # Replace silly paths for LaTeX
 sed -i 's,../content/images/,images/,g' content/*md
 # .. and use SVG instead of PDF
-sed -i 's,\.pdf,.svg,g' content/*md
+#sed -i 's,\.pdf,.svg,g' content/*md
 
 ## Horrible latex seds for the figure captions
 sed -E -i 's/\\ref\{fig:/\{@fig:/g' content/*md
